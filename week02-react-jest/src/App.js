a0bd579fd760c55fbccf593944a9c51be50dad37
daryk at school <daryk.kohler@gmail.com>
2017-04-15 17:40:53 -0700

    Unit Tests with Address assignment completed

--- week02-react-jest/src/App.js
+++ week02-react-jest/src/App.js
@@ -3,28 +3,65 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor(){
-      super();
-      this.state = {
-          nine: '0'
-      }
-  }
-  getNine = () => {
-      console.log('state');
-      this.setState({nine: '9'})
-  };
+    constructor(){
+        super();
+        this.state = {
+            nine: '0',
+            firstName: 'unknown',
+            lastName: 'unknown',
+            home: 'unknown',
+            city: 'unknown',
+            planet: 'unknown',
+            sector: 'unknown'
+        }
+    }
+    getNine = () => {
+        console.log('state');
+        this.setState({nine: '9'})
+    };
+    setAddress = () => {
+        this.setState({
+            firstName: 'Unknown',
+            lastName: 'Palpatine',
+            home: 'Imperial Throne',
+            city: 'Imperial City',
+            planet: 'Imperial Center (Coruscant)',
+            sector: 'Imperial Sector'
+        })
+    };
     render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">Nine: {this.state.nine}</p>
-          <button className="elf" onClick={this.getNine}>Get Nine</button>
-      </div>
-    );
-  }
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Welcome to Prog272</h2>
+                </div>
+                <p className="App-intro">
+                    Nine: {this.state.nine}
+                </p>
+                <p className="App-intro">
+                    firstName: {this.state.firstName}
+                </p>
+                <p className="App-intro">
+                    lastName: {this.state.lastName}
+                </p>
+                <p className="App-intro">
+                    home: {this.state.home}
+                </p>
+                <p className="App-intro">
+                    city: {this.state.city}
+                </p>
+                <p className="App-intro">
+                    planet: {this.state.planet}
+                </p>
+                <p className="App-intro">
+                    sector: {this.state.sector}
+                </p>
+                <button className="elf" onClick={this.getNine}>Get Nine</button>
+                <button className="setAddress" onClick={this.setAddress}>Set Address</button>
+            </div>
+        );
+    }
 }
 
 export default App;
--- week02-react-jest/src/App.test.js
+++ week02-react-jest/src/App.test.js
@@ -3,16 +3,23 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { shallow } from 'enzyme';
 
-describe('jest test', function() {
+describe('React Jest Suite', function () {
 
-    it('renders without crashing', () => {
+    it('renders without our App component without crashing', () => {
         const div = document.createElement('div');
         ReactDOM.render(<App />, div);
     });
 
     it('renders and reads H1 text', () => {
         const wrapper = shallow(<App />);
-        const welcome = <h2>Welcome to React</h2>;
+        const welcome = <h2>Welcome to Prog272</h2>;
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+
+    it('renders and displays the word Nine', () => {
+        const wrapper = shallow(<App />);
+        console.log(wrapper);
+        const welcome = <p className="App-intro">Nine: 0</p>;
         expect(wrapper.contains(welcome)).toEqual(true);
     });
 
@@ -21,6 +28,90 @@ describe('jest test', function() {
         const nineSign = <p className="App-intro">Nine: 9</p>;
         wrapper.find('button.elf').simulate('click');
         expect(wrapper.contains(nineSign)).toEqual(true);
+    });
+
+    it('renders and displays the default first name', () => {
+        const wrapper = shallow(<App />);
+        console.log(wrapper);
+        const welcome = <p className="App-intro">firstName: unknown</p>;
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+
+    it('renders button click message', () => {
+        const wrapper = shallow(<App />);
+        const welcome = <p className="App-intro">firstName: Unknown</p>;
+        wrapper.find('button.setAddress').simulate('click');
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+
+    it('renders and displays the default last name', () => {
+        const wrapper = shallow(<App />);
+        console.log(wrapper);
+        const welcome = <p className="App-intro">lastName: unknown</p>;
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+
+    it('renders button click message', () => {
+        const wrapper = shallow(<App />);
+        const welcome = <p className="App-intro">lastName: Palpatine</p>;
+        wrapper.find('button.setAddress').simulate('click');
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+
+    it('renders and displays the default home', () => {
+        const wrapper = shallow(<App />);
+        console.log(wrapper);
+        const welcome = <p className="App-intro">home: unknown</p>;
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+
+    it('renders button click message', () => {
+        const wrapper = shallow(<App />);
+        const welcome = <p className="App-intro">home: Imperial Throne</p>
+        wrapper.find('button.setAddress').simulate('click');
+        expect(wrapper.contains(welcome)).toEqual(true);
+    })
+
+    it('renders and displays the default city', () => {
+        const wrapper = shallow(<App />);
+        console.log(wrapper);
+        const welcome = <p className="App-intro">city: unknown</p>;
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+
+    it('renders button click message', () => {
+        const wrapper = shallow(<App />);
+        const welcome = <p className="App-intro">city: Imperial City</p>;
+        wrapper.find('button.setAddress').simulate('click');
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+
+    it('renders and displays the default planet', () => {
+        const wrapper = shallow(<App />);
+        console.log(wrapper);
+        const welcome = <p className="App-intro">planet: unknown</p>;
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+
+    it('renders button click message', () => {
+        const wrapper = shallow(<App />);
+        const welcome = <p className="App-intro">planet: Imperial Center (Coruscant)</p>
+        wrapper.find('button.setAddress').simulate('click');
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+
+    it('renders and displays the default sector', () => {
+        const wrapper = shallow(<App />);
+        console.log(wrapper);
+        const welcome = <p className="App-intro">sector: unknown</p>;
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+
+    it('renders button click message', () => {
+        const wrapper = shallow(<App />);
+        const welcome = <p className="App-intro">sector: Imperial Sector</p>
+        wrapper.find('button.setAddress').simulate('click');
+        expect(wrapper.contains(welcome)).toEqual(true);
     });
 
 });
