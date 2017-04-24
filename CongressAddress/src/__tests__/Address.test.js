619af06e4dfdfbeebe688475660224ac51ebc5d0
Daryk on laptop <daryk.kohler@gmail.com>
2017-04-24 10:52:20 -0700

    Tweaked a few tests

--- CongressAddress/src/__tests__/Address.test.js
+++ CongressAddress/src/__tests__/Address.test.js
@@ -5,70 +5,95 @@ import { shallow } from 'enzyme';
 
 describe('React Jest Suite', function () {
 
-
+    const unknown = "unknown";
 
     it('renders and displays the default first name', () => {
         console.log(addresses[0]);
         const wrapper = shallow(<Address addressList={addresses}/>);
-        this.state = addresses[0];
-        const welcome = (<p className="App-intro">
-            firstName: {this.state.firstName}
-        </p>);
+        const welcome = unknown;
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+
+    it('renders and displays the updated first name', () => {
+        const wrapper = shallow(<Address addressList={addresses}/>);
+        wrapper.find('button.setAddress').simulate('click');
+        const welcome = "unknown";
         expect(wrapper.contains(welcome)).toEqual(true);
     });
 
     it('renders and displays the default last name', () => {
         const wrapper = shallow(<Address addressList={addresses}/>);
-        this.state = addresses[0];
-        const welcome = <p className="App-intro">
-            lastName: {this.state.lastName}
-        </p>;
+        const welcome = unknown;
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+
+    it('renders and displays the updated last name', () => {
+        const wrapper = shallow(<Address addressList={addresses}/>);
+        wrapper.find('button.setAddress').simulate('click');
+        const welcome = "Palpatine";
         expect(wrapper.contains(welcome)).toEqual(true);
     });
 
     it('renders and displays the default home', () => {
         const wrapper = shallow(<Address addressList={addresses}/>);
-        this.state = addresses[0];
-        const welcome = <p className="App-intro">
-            home: {this.state.home}
-        </p>;
+        const welcome = unknown;
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+
+    it('renders and displays the updated home', () => {
+        const wrapper = shallow(<Address addressList={addresses}/>);
+        wrapper.find('button.setAddress').simulate('click');
+        const welcome = "Imperial Throne";
         expect(wrapper.contains(welcome)).toEqual(true);
     });
 
     it('renders and displays the default city', () => {
         const wrapper = shallow(<Address addressList={addresses}/>);
-        this.state = addresses[0];
-        const welcome = <p className="App-intro">
-            city: {this.state.city}
-        </p>;
+        const welcome = unknown;
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+
+    it('renders and displays the updated city', () => {
+        const wrapper = shallow(<Address addressList={addresses}/>);
+        wrapper.find('button.setAddress').simulate('click');
+        const welcome = "Imperial City";
         expect(wrapper.contains(welcome)).toEqual(true);
     });
 
     it('renders and displays the default planet', () => {
         const wrapper = shallow(<Address addressList={addresses}/>);
-        this.state = addresses[0];
-        const welcome = <p className="App-intro">
-            planet: {this.state.planet}
-        </p>;
+        const welcome = unknown;
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+
+    it('renders and displays the updated planet', () => {
+        const wrapper = shallow(<Address addressList={addresses}/>);
+        wrapper.find('button.setAddress').simulate('click');
+        const welcome = "Imperial Center (Coruscant)";
         expect(wrapper.contains(welcome)).toEqual(true);
     });
 
     it('renders and displays the default sector', () => {
         const wrapper = shallow(<Address addressList={addresses}/>);
-        this.state = addresses[0];
-        const welcome = <p className="App-intro">
-            sector: {this.state.sector}
-        </p>;
+        const welcome = unknown;
         expect(wrapper.contains(welcome)).toEqual(true);
     });
 
-    it('renders button click message', () => {
+    it('renders and displays the updated sector', () => {
         const wrapper = shallow(<Address addressList={addresses}/>);
         wrapper.find('button.setAddress').simulate('click');
-        for (var variable in addresses[1]) {
-            addresses[1][variable] = <Address addresses={addresses}/>;
-        }
+        const welcome = "Imperial Sector";
+        expect(wrapper.contains(welcome)).toEqual(true);
     });
+
+    /*for (var variable in addresses[1]) {
+        it.only('renders button click message', () => {
+            const wrapper = shallow(<Address addressList={addresses}/>);
+            wrapper.find('button.setAddress').simulate('click');
+            addresses[1][variable] = <Address addresses={addresses}/>;
+            expect(wrapper.contains(addresses)).toEqual(true);
+        });
+    }*/
 });
 
 /**
