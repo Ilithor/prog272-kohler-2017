8562e7aa9f7cbbff233099cf637a47f09d66ce53
Daryk on laptop <daryk.kohler@gmail.com>
2017-06-11 16:54:56 -0700

    Made updates to CongressServer.

--- CongressServer/routes/index.js
+++ CongressServer/routes/index.js
@@ -4,22 +4,8 @@ let router = express.Router();
 let allMongo = require('./all-mongo');
 let connect = require('./connect');
 
-router.get('/', function(req, res, next) {
-    'use strict';
-    res.render('index', {title: 'CongressServer'});
-});
-
-router.get('/bar', function(request, response) {
-    response.status(200).send({result: 'bar'});
-});
-
-router.get('/:id', function(request, response) {
-    'use strict';
-    response.status(200).send({result: request.params.id});
-});
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
     'use strict';
     res.render('index', {title: 'CongressServer'});
 });
@@ -32,6 +18,40 @@ router.get('/bar', function(request, response) {
 router.get('/bar/:id', function(request, response) {
     'use strict';
     response.status(200).send({param: request.params.id});
+});
+
+function checkConnection() {
+    if (!connect.connected) {
+        connect.doConnection('mlab');
+    }
+}
+
+router.get('/all-data', function(request, response) {
+    'use strict';
+    console.log('AllData route invoked.');
+    checkConnection();
+    allMongo.getAllData(response);
+});
+
+router.get('/emptyCollection', function(request, response) {
+    'use strict';
+    checkConnection();
+    allMongo.empty(response);
+});
+
+router.get('/insertValidCollection', function(request, response) {
+    'use strict';
+    console.log('Insert Valid Collection Called.');
+    response.status(200).send({result: 'Insert valid Collection'});
+    //checkConnection();
+    //allMongo.readDataAndInsert(response);
+});
+
+router.get('/update', function(request, response) {
+    'use strict';
+    checkConnection();
+    console.log('request query', request.query);
+    allMongo.update(request.query, response);
 });
 
 module.exports = router;
